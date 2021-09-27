8e84a6fd5a342508ef7b7122b4d0e8a566886762
menyagah <menyagah27@gmail.com>
2021-09-27 18:01:04 +0300

    Implement checkbox true or false completed

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,13 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   mode: 'production',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
